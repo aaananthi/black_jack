a39607ffa543f400c3537da97ae0c1148c7dfcdf
Aananthi J S <167883496+aaananthi@users.noreply.github.com>
2025-09-13 10:22:22 +0530

    Updated card.py

--- card.py
+++ card.py
@@ -1,13 +1,33 @@
 class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
+    SUIT_SYMBOLS = {
+        0: u"\u2666",  # diamonds
+        1: u"\u2665",  # hearts
+        2: u"\u2663",  # clubs
+        3: u"\u2660"  # spades
+    }
+
+    VALUE_NAMES = {
+        1: "A",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+        6: "6",
+        7: "7",
+        8: "8",
+        9: "9",
+        10: "T",
+        11: "J",
+        12: "Q",
+        13: "K"
+    }
+
+    def __init__(self, suit, value):
         self.suit = suit
+        self.value = value
+        self.hidden = False
 
-    def display_card(self):
-        return (f"{self.rank} of {self.suit}")
-
-    def is_ace():
-        pass
-
-    def is_face_card():
-        pass
+    def __str__(self):
+        if self.hidden:
+            return "Unknown"
+        return f"{self.SUIT_SYMBOLS[self.suit]}{self.VALUE_NAMES[self.value]}"
